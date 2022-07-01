5937cce1297631bcc42d2a6e5ce8cd27f76f026d
hzl <13071653942@163.com>
2022-07-01 15:59:00 +0800

    first commit

---
+++ src/main/java/com/hzl/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.hzl.git;
+
+/**
+ * @Author hzl
+ * @create 2022/7/1 15:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
